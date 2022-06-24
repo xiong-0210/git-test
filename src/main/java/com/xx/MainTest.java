532dd60515bf23b9048e4257b0d8d18c738fdda0
17276 <1727618243@qq.com>
2022-06-24 18:49:45 +0800

    hot-fix  commit

--- src/main/java/com/xx/MainTest.java
+++ src/main/java/com/xx/MainTest.java
@@ -5,5 +5,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hot -fix");
     }
 }
